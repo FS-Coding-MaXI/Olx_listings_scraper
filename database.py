f3ff39e3d1ca921c84503d2ba8ac08923011ede6
HermanPlay <hermanplay1110@gmail.com>
2023-03-10 23:49:06 +0100

    Created database class

--- database.py
+++ database.py
@@ -1,13 +1,21 @@
 import pymongo
+from single_listing import Listing
+import asyncio
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+class Database(pymongo.MongoClient):
+    def __init__(self, user, password):
+        super().__init__(
+            f"mongodb+srv://{user}:{password}@olx-scrapper.hpl6bsu.mongodb.net/?retryWrites=true&w=majority"
+        )
 
-mydb = myclient["listings_db"]
+        self.db = self["olx-scrapper"]
 
-mycol = mydb["listings"]
+        self.listing_collection = self.db["listings"]
 
-mydict = { "name": "John", "address": "Highway 37" }
-
-x = mycol.insert_one(mydict)
-
-print(x.inserted_id)
+    async def ifExists(self, listing: Listing):
+        if self.listing_collection.find({'link': listing.link}).count() > 0:
+            return True
+        return False
+    
+    async def add_listing(self, listing: Listing):
+        self.listing_collection.insert_one(listing)
