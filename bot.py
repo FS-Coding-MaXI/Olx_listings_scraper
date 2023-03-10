590d4611d7df6166d0cf5458c72861b0bb0eb6a6
HermanPlay <hermanplay1110@gmail.com>
2023-03-10 17:08:07 +0100

    Fix linter

--- bot.py
+++ bot.py
@@ -9,30 +9,34 @@ scraper = OlxScraper()
 listings = Listings()
 scraper.start_session()
 
+
 class MyBot(commands.Bot):
     def __init__(self, command_prefix: str, intents: discord.Intents, self_bot: bool):
-        super().__init__(command_prefix=command_prefix, intents=intents, self_bot=self_bot)
+        super().__init__(
+            command_prefix=command_prefix, intents=intents, self_bot=self_bot
+        )
         self.add_commands()
-    
+
     async def on_ready(self):
         channel = self.get_channel(1082965800400474164)
         await channel.send("I have started! ")
         self.scrap.start()
-    
+
     def add_commands(self):
         @self.command(name="status", pass_context=True)
         async def status(ctx):
             print(ctx)
             await ctx.channel.send("Kurwa dzialam " + ctx.author.name)
+
         @self.command(name="ping")
         async def ping(ctx):
             await ctx.send("pong")
-    
+
     async def send_listing(self, listing) -> None:
         channel = self.get_channel(1083725146080149524)
         message = f"UWAGA - {listing.listing_name}\nCena: **{listing.price}**\nOgloszenie - {listing.link}"
         await channel.send(message)
-    
+
     @tasks.loop(minutes=1)
     async def scrap(self):
         scraper.get_all_pages()
@@ -44,12 +48,13 @@ class MyBot(commands.Bot):
                 await self.send_listing(listing)
                 listing.sent2discord = True
 
-   
+
 def main():
     intents = discord.Intents.all()
     bot = MyBot(command_prefix="-", intents=intents, self_bot=False)
 
     bot.run(TOKEN)
+
 
 if __name__ == "__main__":
     main()
