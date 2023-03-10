9aaa63e45708b85b94501c1215f76a1b99089bc4
HermanPlay <hermanplay1110@gmail.com>
2023-03-10 17:12:59 +0100

    Added comments

--- bot.py
+++ bot.py
@@ -17,32 +17,39 @@ class MyBot(commands.Bot):
         )
         self.add_commands()
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         channel = self.get_channel(1082965800400474164)
         await channel.send("I have started! ")
         self.scrap.start()
 
     def add_commands(self):
         @self.command(name="status", pass_context=True)
-        async def status(ctx):
+        async def status(ctx) -> None:
             print(ctx)
             await ctx.channel.send("Kurwa dzialam " + ctx.author.name)
 
         @self.command(name="ping")
-        async def ping(ctx):
+        async def ping(ctx) -> None:
             await ctx.send("pong")
 
+    # Takes listing and sends to specific channel
     async def send_listing(self, listing) -> None:
-        channel = self.get_channel(1083725146080149524)
-        message = f"UWAGA - {listing.listing_name}\nCena: **{listing.price}**\nOgloszenie - {listing.link}"
-        await channel.send(message)
+        channel = self.get_channel(
+            1083725146080149524
+        )  # Specify channel to which we want to send message
+        message = f"UWAGA - {listing.listing_name}\nCena: **{listing.price}**\nOgloszenie - {listing.link}"  # Format message
+        await channel.send(message)  # Send formatted message
 
+    # Background loop which every minute checks if there is new listng if so, post it
     @tasks.loop(minutes=1)
-    async def scrap(self):
+    async def scrap(self) -> None:
+        # Scraping process
         scraper.get_all_pages()
         for p in range(scraper.page_number):
             listings.add_unv_listings(scraper.get_listings_from_page(p))
             listings.validate_listings()
+
+        # After validating for each listing which was not posted, call send_listing, and change bool parameter.
         for listing in listings.val_offers:
             if not listing.sent2discord:
                 await self.send_listing(listing)
@@ -52,7 +59,6 @@ class MyBot(commands.Bot):
 def main():
     intents = discord.Intents.all()
     bot = MyBot(command_prefix="-", intents=intents, self_bot=False)
-
     bot.run(TOKEN)
 
 
