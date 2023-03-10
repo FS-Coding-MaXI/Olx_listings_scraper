07f9f412dcf387d1e2eae73a40d8fd572360fe12
HermanPlay <hermanplay1110@gmail.com>
2023-03-10 19:41:55 +0100

    Added seperate channel for logs and sinding embeds

--- bot.py
+++ bot.py
@@ -15,13 +15,20 @@ class MyBot(commands.Bot):
             command_prefix=command_prefix, intents=intents, self_bot=self_bot
         )
         self.add_commands()
+        self.LOG_CHANNEL = 1083820031630114927
+        self.LISTING_CHANNEL = 1083725146080149524
 
     async def on_ready(self) -> None:
-        channel = self.get_channel(1082965800400474164)
+        channel = self.get_channel(self.LOG_CHANNEL)
         await channel.send("I have started! ")
-        self.scrap.start()
+        # self.scrap.start()
+        await self.scrap()
 
     def add_commands(self):
+        @self.command(name="test_list", pass_context=True)
+        async def test_list(ctx) -> None:
+            self.send_listing(listings.val_offers[0])
+
         @self.command(name="status", pass_context=True)
         async def status(ctx) -> None:
             print(ctx)
@@ -31,16 +38,29 @@ class MyBot(commands.Bot):
         async def ping(ctx) -> None:
             await ctx.send("pong")
 
+    async def log_listing(self, listing) -> None:
+        channel = self.get_channel(self.LOG_CHANNEL)
+        await channel.send(f"Sending new listing {listing.link}")
+
     # Takes listing and sends to specific channel
     async def send_listing(self, listing) -> None:
         channel = self.get_channel(
-            1083725146080149524
+            self.LISTING_CHANNEL
         )  # Specify channel to which we want to send message
-        message = f"UWAGA - {listing.listing_name}\nCena: **{listing.price}**\nOgloszenie - {listing.link}"  # Format message
-        await channel.send(message)  # Send formatted message
+
+        embed = discord.Embed(
+            title=f"{listing.listing_name}", url=f"{listing.link}", color=0x50D68D
+        )
+        embed.set_thumbnail(url="https://i.imgur.com/6yk5bMu.jpeg")
+        embed.add_field(name="CENA: ", value=f"**{listing.price}**", inline="false")
+        embed.set_footer(
+            text="by OLX Scraper", icon_url="https://i.imgur.com/hUhyNEL.jpeg"
+        )
+        # message = f"UWAGA - {listing.listing_name}\nCena: **{listing.price}**\nOgloszenie - {listing.link}"  # Format message
+        await channel.send(embed=embed)  # Send formatted message
 
     # Background loop which every minute checks if there is new listng if so, post it
-    @tasks.loop(minutes=1)
+    # @tasks.loop(minutes=1)
     async def scrap(self) -> None:
         # Scraping process
         scraper.get_all_pages()
@@ -51,8 +71,10 @@ class MyBot(commands.Bot):
         # After validating for each listing which was not posted, call send_listing, and change bool parameter.
         for listing in listings.val_offers:
             if not listing.sent2discord:
+                await self.log_listing(listing)
                 await self.send_listing(listing)
                 listing.sent2discord = True
+                break
 
 
 def main():
